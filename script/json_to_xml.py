8b7462172a7146d64bc8b250cddef6d01e94354d
Larouyan <ylaaroussi@ymail.com>
2022-03-02 19:22:33 +0100

    some progress

--- script/json_to_xml.py
+++ script/json_to_xml.py
@@ -1,3 +1,43 @@
+import glob
+import os
+from lxml import etree as ET
+import argparse
+import re
+import json
+
+
+def convert_json_to_xml(file_to_process):
+    """
+        this function take a json file containing data about Tumor, Extraepithelial CD8+ Cell and
+        Intraepithelial CD8+ Cell as input and create a xml file that can be loaded in ASAP.
+    """
+    # interested areas
+    annotations = ['Tumor', 'Extraepithelial CD8+ Cell', 'Intraepithelial CD8+ Cell']
+    # color code of the interested areas
+    colors = {'Tumor': '#4d66cc', 'Extraepithelial CD8+ Cell': 'magenta', 'Intraepithelial CD8+ Cell': 'magenta'}
+
+    # initiate xml_tree
+    xml_tree = ET.Element('ASAP_Annotations')
+    # Make the annotations and coordinates
+    xml_annotations = ET.SubElement(xml_tree, 'Annotations')
+    # Make the groups
+    xml_annotation_groups = ET.SubElement(xml_tree, 'AnnotationGroups')
+
+    # open json file
+    with open(file_to_process) as file:
+        # load json file
+        data = json.load(file)
+        for group in annotations:
+            for object in data['Objects_Data']:
+                if object['Classification'] == group:
+                    
+
+
+
+    # print(ET.tostring(xml_tree))
+
 
 if __name__ == '__main__':
-    print("hello World")
+    path = 'C:/Users/Maintenant Pret/Desktop/Unifr/Bachelor/data/' \
+           'export/Masks_00.2205_1D_AE1_AE3_CD8-level0-hotspot.json'
+    convert_json_to_xml(path)
