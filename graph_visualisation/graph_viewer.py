4572925ef3070e547762339a20b429d2a99ee059
Larouyan <ylaaroussi@ymail.com>
2022-04-10 10:36:19 +0200

    progress...

--- graph_visualisation/graph_viewer.py
+++ graph_visualisation/graph_viewer.py
@@ -1,10 +1,50 @@
 from tkinter import *
-from tkinter import colorchooser
-from tkinter import ttk
+from tkinter import colorchooser, filedialog, ttk
+from PIL import ImageTk, Image
+
+import os
+
+
+def select_img_dir():
+    new_dir = filedialog.askdirectory()
+    if new_dir:
+        if os.path.isdir(new_dir):
+            img_dir.set(new_dir)
+
+
+def select_gxl_dir():
+    new_dir = filedialog.askdirectory()
+    if new_dir:
+        if os.path.isdir(new_dir):
+            load_gxl_dir(new_dir)
+
+
+def load_gxl_dir(new_dir):
+    gxl_dir.set(new_dir)
+    # delete previous gxl dir
+    listbox.delete(0, END)
+    for file in os.listdir(new_dir):
+        if file.lower().endswith('.gxl'):
+            if os.path.isfile(os.path.join(new_dir, file)):
+                listbox.insert(END, file.rsplit('.', 1)[0])
+    listbox.select_set(0)
+    # onselect()
+
+
+def save_img():
+    new_dir = filedialog.askdirectory()
+    if new_dir:
+        if os.path.isdir(new_dir):
+            pass
 
 
 def change_color():
     return colorchooser.askcolor()
+
+
+def onselect():
+    pass
+    # todo
 
 
 if __name__ == '__main__':
@@ -19,10 +59,39 @@ if __name__ == '__main__':
 
     mainframe = ttk.Frame(root, padding="3 3 12 12")
     mainframe.grid(column=0, row=0)
-    mainframe.columnconfigure(2, weight=1)
-    mainframe.rowconfigure(2, weight=1)
+    mainframe.columnconfigure(3, weight=1)
+    mainframe.rowconfigure(3, weight=1)
     mainframe.pack(expand=1, fill='both')
 
+    img_dir = StringVar()
+    ttk.Button(mainframe, text='Image Path:', command=select_img_dir).grid(column=0, row=0, columnspan=2)
+    ttk.Label(mainframe, textvariable=img_dir).grid(column=2, row=0)
+
+    gxl_dir = StringVar()
+    ttk.Button(mainframe, text='GXL Path:', command=select_gxl_dir).grid(column=0, row=1, columnspan=2)
+    ttk.Label(mainframe, textvariable=gxl_dir).grid(column=2, row=1)
+
+    # Create listbox to list all the gxl files
+    listbox = Listbox(mainframe, height=5)
+    listbox.grid(column=0, row=2)
+    scrollbar = ttk.Scrollbar(mainframe, orient=VERTICAL, command=listbox.yview)
+    scrollbar.grid(column=1, row=2)
+    listbox['yscrollcommand'] = scrollbar.set
+    # listbox.bind('<<ListboxSelect>>', onselect)
+
+    # Canvas to display images
+    # canvas_state is True if the canvas is not empty
+    canvas_state = BooleanVar()
+    canvas_state.set(False)
+    canvas = Canvas(mainframe, bg='white')
+    canvas.grid(column=2, row=2)
+
+    # Button to save image
+    save_button = ttk.Button(mainframe, text='Save As', command=save_img, state=DISABLED)
+    save_button.grid(column=0, row=3, columnspan=2)
+
+    # Radio Buttons for color_by_feature
+    feature = StringVar
     
 
     root.mainloop()
